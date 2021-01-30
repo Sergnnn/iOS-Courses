f186d03dbf90e3d00f2045e5d5d8d2b4099692e9
SNA <drebin96@gmail.com>
2021-01-30 14:35:28 +0200

    Course 3 finished

---
+++ iOS Dev 2.0/Xcodes/Alias-First-App/Alias-First-App/Data/Data.swift
@@ -0,0 +1,18 @@
+//
+//  Data.swift
+//  Alias-First-App
+//
+//  Created by Сергей Непомящий on 29.01.2021.
+//
+
+import Foundation
+
+struct word {
+    var categoryName: String
+    var word: String
+}
+
+var words = [word]()
+
+
+
---
+++ iOS Dev 2.0/Xcodes/Alias-First-App/Alias-First-App/View/WordsCategoryCell.swift
@@ -0,0 +1,22 @@
+//
+//  WordsCategoryCell.swift
+//  Alias-First-App
+//
+//  Created by Сергей Непомящий on 29.01.2021.
+//
+
+import UIKit
+
+class WordsCategoryCell: UITableViewCell {
+
+    @IBOutlet weak var wordsCategoryName: UILabel!
+    @IBOutlet weak var switchValue: UISwitch!
+    
+//    func configureCell(goal: Goal  ) {
+//        self.goalDescriptionLabel.text = goal.goalDescription
+//        self.goalTypeLabel.text = goal.goalType
+//        self.goalProgressLabel.text = String(goal.goalProgress)
+//
+//    }
+
+}
