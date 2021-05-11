0006642251cad03f9b745b1077aea87a6b22dc3a
SNA <drebin96@gmail.com>
2021-05-11 14:19:56 +0300

    new folder

--- iOS Dev 2.0/CodeWars/reverseString.playground/Contents.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-var str = "Hello, playground"
-
-func reverse(_ str: String) -> String {
-    return String(str.reversed())
-}
-
-reverse(str)
--- iOS Dev 2.0/Playgrounds/Different/Sorting Array in alphabetical way.playground/Contents.swift
+++
@@ -1,6 +0,0 @@
-import UIKit
-
-var arrayToSort = ["K","B","C","A"]
-
-arrayToSort.sort {$0 < $1}
-arrayToSort
