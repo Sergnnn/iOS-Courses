701985054b936fb94685c8fb49f60b4962945305
SNA <drebin96@gmail.com>
2021-08-11 13:43:11 +0300

    window shopper add

---
+++ iOS-Dev-2.0/Xcodes/windowshoppperapp/window-shopper/Controller/MainVC.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  window-shopper
+//
+//  Created by Mark Price on 6/19/17.
+//  Copyright © 2017 Devslopes. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    @IBOutlet weak var wageTxt: CurrencyTxtField!
+    @IBOutlet weak var priceTxt: CurrencyTxtField!
+    
+    @IBOutlet weak var resultLbl: UILabel!
+    @IBOutlet weak var hoursLbl: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let calcBtn = CalcBtn(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
+        
+        calcBtn.addTarget(self, action: #selector(MainVC.calculate), for: .touchUpInside)
+        
+        wageTxt.inputAccessoryView = calcBtn
+        priceTxt.inputAccessoryView = calcBtn
+        
+        resultLbl.isHidden = true
+        hoursLbl.isHidden = true
+    }
+
+    @objc func calculate() {
+        if let wageTxt = wageTxt.text, let priceTxt = priceTxt.text {
+            if let wage = Double(wageTxt), let price = Double(priceTxt) {
+                view.endEditing(true)
+                resultLbl.isHidden = false
+                hoursLbl.isHidden = false
+                resultLbl.text = "\(Wage.getHours(forWage: wage, andPrice: price))"
+            }
+        }
+    }
+
+    @IBAction func clearCalculatorPressed(_ sender: Any) {
+        resultLbl.isHidden = true
+        hoursLbl.isHidden = true
+        wageTxt.text = ""
+        priceTxt.text = ""
+    }
+    
+}
+
